720ccb2c8e61326583230ac14dbd62d1e451aabb
pratik-first-demat <pratik@firstdemat.in>
2026-06-27 02:46:45 +0530

    [Domain/User] Supabase CLI added. User entity created

---
+++ supabase/migrations/20260626211253_create_users.sql
@@ -0,0 +1,10 @@
+create table users (
+    id uuid primary key default gen_random_uuid(),
+
+    email text not null unique,
+    password_hash text not null,
+    full_name text not null,
+
+    created_at timestamptz not null default now(),
+    updated_at timestamptz not null default now()
+);
